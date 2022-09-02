4a8fb8e8692af13c4326f638885d9bfcae3696ca
yuriscavalcante <yuri.scavalcante@outlook.com>
2022-09-02 12:32:58 -0300

    Resolução feita

--- desafio1/src/App.js
+++ desafio1/src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Desafio 1</h1>
+      <h2>Resolução do desafio!</h2>
     </div>
   );
 }
